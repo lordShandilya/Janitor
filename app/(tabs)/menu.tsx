924688f1273d12d26c6a9ffac4140c479918180b
sahityaaryan <sahityaaryansingh@gmail.com>
2025-01-21 03:24:31 +0530

    Menu tab

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,30 +1,89 @@
-import { ThemedText } from "@/components/ThemedText"
-import { StyleSheet, View } from "react-native"
-
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import MenuScroller from "@/components/MenuScrolller";
+import DropDown from "@/components/DropDown";
+o
 export default function Menu() {
-    return ( 
-        <View style={styles.titleContainer}>
-            <ThemedText type="default">Under development...</ThemedText>
-        </View>
-    )
-}
 
-const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 8,
-        marginTop: 50
-      },
-      stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-      },
-      reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-      },
-})
+
+  const breakfast = [
+    "Aalu chana",
+    "CornFlakes(milk)",
+    "Tea",
+    "Bread",
+    "Omelette",
+    "Poha",
+    "Idli",
+    "Dosa",
+    "Idli",
+    "Dosa",
+  ];
+
+
+  const lunch = [
+    "Aalu chana",
+    "CornFlakes(milk)",
+    "Tea",
+    "Bread",
+    "Omelette",
+    "Poha",
+    "Idli",
+    "Dosa",
+    "Idli",
+    "Dosa",
+  ];
+  const dinner = [
+    "Aalu chana",
+    "CornFlakes(milk)",
+    "Tea",
+    "Bread",
+    "Omelette",
+    "Poha",
+    "Idli",
+    "Dosa",
+    "Idli",
+    "Dosa",
+  ];
+
+
+  return (
+    <View className="w-full bg-white h-full justify-center items-center">
+      
+      <View className=" my-8 z-10"> 
+
+        <DropDown/>
+      </View>
+
+
+
+      <View className=" h-1/4 w-[94%] flex items-center">
+        <Text className="text-blue-500 font-semibold font-poppins-medium text-lg">Breakfast</Text>
+
+        <View className="flex flex-col h-full w-full">
+          <MenuScroller list={breakfast} />
+        </View>
+      </View>
+
+      <View className=" h-1/4 w-[94%] flex items-center">
+        <Text className="text-blue-500 font-semibold font-poppins-medium text-lg">Lunch</Text>
+
+        <View className="flex flex-col h-full w-full">
+          <MenuScroller list={lunch} />
+        </View>
+      </View>
+
+      <View className=" h-1/4 w-[94%] flex items-center">
+        <Text className="text-blue-500 font-semibold font-poppins-medium text-lg">Dinner</Text>
+
+        <View className="flex flex-col h-full w-full">
+          <MenuScroller list={dinner} />
+        </View>
+      </View>
+
+
+      
+
+    </View>
+  );
+}
